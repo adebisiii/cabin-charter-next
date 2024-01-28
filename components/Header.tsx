d4676c26ed29b49643a7d78cda87fa18625e4497
adebisiii <34288825+adebisiii@users.noreply.github.com>
2024-01-29 01:47:18 +0300

    navbar scrolling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Logo from "./Logo";
-import { Button } from "./ui/button";
 import Navbar from "./Navbar";
 import ToggleMenu from "./ToggleMenu";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+
+      // Belirli bir scroll değerine göre durumu kontrol et
+      const shouldChangeStyle = scrollTop > 100; // Örneğin, 100 piksel'den sonra değişiklik yap
+
+      setIsScrolled(shouldChangeStyle);
+    };
+
+    // Scroll olayını dinle
+    window.addEventListener("scroll", handleScroll);
+
+    // Temizleme
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="transparent-header fixed left-0 top-0 z-[5] transition-all flex w-full justify-between bg-white px-4 py-3.5 sm:bg-transparent sm:px-6 lg:py-6 2xl:px-7 3xl:px-8 4xl:px-16 4xl:py-9">
-      <Logo />
+    <header
+      className={`fixed left-0 top-0 z-[5] transition-all flex w-full justify-between px-4 py-2 sm:px-6 lg:py-5 2xl:px-7 3xl:px-8 4xl:px-16 4xl:py-6 ${
+        isScrolled ? "isScrolling" : ""
+      }`}
+    >
+      <Logo/>
       <div className="flex items-center">
         <Navbar />
       </div>
