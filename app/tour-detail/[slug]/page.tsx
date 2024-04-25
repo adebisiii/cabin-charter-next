240ad06b9c174308a9380226283d2c021eb13e44
adebisiii <34288825+adebisiii@users.noreply.github.com>
2024-04-26 02:00:59 +0300

    tour details

--- app/tour-detail/[slug]/page.tsx
+++ app/tour-detail/[slug]/page.tsx
@@ -1,10 +1,12 @@
+import TourDetailCarousel from "@/components/TourDetailCarousel"
+
 const TourDetail = () => {
   return (
     <main className="w-full md:py-20">
       <div className="w-full max-w-[1280px] px-5 md:px-10 mx-auto">
         <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
           <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
-            left
+            <TourDetailCarousel />
           </div>
           <div className="flex-1 py-3">right</div>
         </div>
---
+++ components/TourDetailCarousel.tsx
@@ -0,0 +1,8 @@
+
+const TourDetailCarousel = () => {
+  return (
+    <div>TourDetailCarousel</div>
+  )
+}
+
+export default TourDetailCarousel
