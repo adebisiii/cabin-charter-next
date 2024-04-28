d1527d6027be2ae4527694de0046f8c8d0fed461
adebisiii <34288825+adebisiii@users.noreply.github.com>
2024-04-28 15:45:17 +0300

    tour details

--- app/tour-detail/[slug]/page.tsx
+++ app/tour-detail/[slug]/page.tsx
@@ -1,13 +1,14 @@
 import TourDetailCarousel from "@/components/TourDetailCarousel"
-import { User } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Minus, Plus, User } from "lucide-react"
 
 const TourDetail = () => {
   return (
     <main className="w-full py-28">
-      <div className="max-w-[1440px] px-5 md:px-10 mx-auto">
+      <div className="px-5 md:px-10 mx-auto">
         <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
           {/* left column start */}
-          <div className="w-full md:w-auto flex- max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
             <TourDetailCarousel />
           </div>
           {/* left column end */}
@@ -24,47 +25,124 @@ const TourDetail = () => {
               <p className="text-xl font-semibold ml-2">€ 700</p>
             </div>
             {/* Tour Subtitle */}
-            <div className="text-sm mb-5 leading-7 text-gray-800">
+            <div className="text-sm mb-2 leading-7 text-gray-800">
               <p className="py-3">This yacht cruise is run by standard gulet yachts and departs every four days of the week during the season. By joining this yacht cruise you will explore the Lycian Coast which is one of the most beautiful coast line in the Mediterranean.</p>
             </div>
 
             {/* Tour Avaliable Dates Start */}
-            <div className="mb-10">
-              <div className="flex mb-2 text-md font-semibold">
-                Available Dates / Guests
-              </div>
-              <div className="grid grid-cols-3 gap-2 text-sm">
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / 8
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / 8
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / 11
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium">
-                  24.09.2024 / Left 8 Guests
+            <div className="border rounded-md p-6 shadow-md">
+            <div className="flex items-center justify-between py-2">
+                <p className="text-md font-semibold">Number of People </p>
+                <div className="flex items-center gap-4">
+                  <Button variant="outline" size="icon">
+                    <Minus className="w-5 h-5" />
+                  </Button>
+                  <p className="text-center font-normal">0</p>
+                  <Button variant="outline" size="icon">
+                    <Plus className="w-5 h-5" />
+                  </Button>
                 </div>
               </div>
+              <div className="flex mt-4 mb-2 text-md font-semibold">
+                Select Date
+              </div>
+              <div className="grid grid-cols-3 md:grid-cols-5 gap-2 text-sm">
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">11.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">18.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">25.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€ 1190</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium">
+                  <span className="block">04.05.2024</span>
+                  <span className="block">€700</span>
+                </div>
+              </div>
+              <Button className="mt-4 w-full py-6 text-base">RESERVE</Button>
+              <ul className="mt-3 xl:mt-5">
+                <li className="flex items-center justify-between py-1.5 text-base first:pt-0 last:border-t last:border-gray-300 last:pb-0">
+                  <span className="font-normal">€ 700 * 2 Guest(s)</span>
+                  <span className="font-bold">€ 1400</span>
+                </li>
+                <li className="flex items-center justify-between py-1.5 text-base first:pt-0 last:border-t last:border-gray-300 last:pb-0">
+                  <span className="font-normal">Discount</span>
+                  <span className="font-bold text-red-600">- € 200</span>
+                </li>
+                <li className="flex items-center justify-between py-1.5 text-base first:pt-0 last:border-t last:border-gray-300 last:pb-0">
+                  <span className="font-normal">Total</span>
+                  <span className="font-bold">€ 1200</span>
+                </li>
+              </ul>
             </div>
 
           </div>
--- components/TourDetailCarousel.tsx
+++ components/TourDetailCarousel.tsx
@@ -5,21 +5,21 @@ const TourDetailCarousel = () => {
     <div className="lg:col-span-3 lg:row-end-1">
     <div className="lg:flex lg:items-start">
       <div className="lg:order-2 lg:ml-5">
-        <div className="max-w-xl overflow-hidden rounded-lg">
-          <Image width={500} height={500} className="h-full w-full max-w-full object-cover" src="/images/boat-twelve.webp" alt="" />
+        <div className="w-full overflow-hidden rounded-lg">
+          <Image width={600} height={800} className="h-full w-full max-w-full object-cover" src="/images/antalya-kekova-cruise.jpg" alt="" />
         </div>
       </div>
 
       <div className="mt-2 w-full lg:order-1 lg:w-32 lg:flex-shrink-0">
         <div className="flex flex-row items-start lg:flex-col">
           <button type="button" className="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-gray-700 text-center">
-            <Image width={120} height={120} className="h-full w-full object-cover" src="/images/boat-twelve.webp" alt="" />
+            <Image width={180} height={180} className="h-full w-full object-cover" src="/images/antalya-kekova-cruise.jpg" alt="" />
           </button>
           <button type="button" className="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
-            <Image width={120} height={120} className="h-full w-full object-fill " src="/images/boat-twelve.webp" alt="" />
+            <Image width={180} height={180} className="h-full w-full object-fill " src="/images/antalya-kekova-cruise.jpg" alt="" />
           </button>
           <button type="button" className="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
-            <Image width={120} height={120} className="h-full w-full object-cover" src="/images/boat-twelve.webp" alt="" />
+            <Image width={180} height={180} className="h-full w-full object-cover" src="/images/antalya-kekova-cruise.jpg" alt="" />
           </button>
         </div>
       </div>
